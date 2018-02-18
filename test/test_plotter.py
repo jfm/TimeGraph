f783649d5bd189413bdc0334d27d2db06d9deb68
Jesper Fussing Moerk <jfm@moerks.dk>
2018-02-18 18:14:52 +0100

    More work on plotter - Initial working example now done

--- test/test_plotter.py
+++ test/test_plotter.py
@@ -1,4 +1,4 @@
-import unittest
+from nose.tools import assert_equals
 from timegraph.drawing.plotter import Plotter
 
 test_values = [('2015-08-18T00:00:00Z', 4.354952083333334), ('2015-08-19T00:00:00Z', 4.308158333333333),
@@ -20,6 +20,13 @@ test_values = [('2015-08-18T00:00:00Z', 4.354952083333334), ('2015-08-19T00:00:0
 
 test_values_numbers_only = [(number) for (time, number) in test_values]
 
-def test_plot_timeseries():
+# def test_plot_timeseries():
+#     plotter = Plotter()
+#     plotter.plot_timeseries(test_values_numbers_only)
+
+def test_get_y_scale():
+    values = [(4.354952083333334, 7), (4.308158333333333, 7), (4.302979166666666, 7), (4.311062499999999, 7), (4.253908333333332, 7), (4.189418750000003, 7), (4.153933333333331, 7), (4.172108333333331, 7), (4.261525, 7), (4.292508333333336, 7), (4.4140041666666665, 7), (4.499274999999998, 8), (4.439795833333335, 8), (4.4404875000000015, 8), (4.42244375, 8), (4.421910416666667, 8), (4.410592436974791, 7), (4.3801125, 7), (4.318624217118998, 7), (4.288097916666669, 7), (4.393808333333334, 7), (4.476208333333333, 8), (4.569668750000003, 8), (4.60123125, 8), (4.608704166666667, 8), (4.658435416666667, 8), (4.7641395833333355, 8), (4.911522916666667, 9), (4.857977083333336, 8), (4.680843749999998, 8), (4.603674999999998, 8), (4.37027676240209, 7)]
     plotter = Plotter()
-    plotter.plot_timeseries(test_values_numbers_only)
+    y_scale = plotter.get_y_scale(values)
+    assert_equals(max(y_scale), '4.91 ')
+    assert_equals(min(y_scale), '2.64 ')
--- timegraph/drawing/plotter.py
+++ timegraph/drawing/plotter.py
@@ -2,15 +2,16 @@ class Plotter():
 
     def __init__(self):
         self.height = 10
-        self.width = 40
+        self.width = 80
 
     def plot_timeseries(self, value_list):
-        #print(value_list)
-        x_values = self.scale_values(value_list)
-        #print(x_values)
+        scaled_values = self.scale_values(value_list)
+        y_scale = self.get_y_scale(scaled_values)
+
+        x_values = [scaled for (original, scaled) in scaled_values]
 
         matrix = self.generate_matrix(x_values)
-        self.print_matrix(matrix)
+        self.print_matrix(y_scale, matrix)
 
     def scale_values(self, value_list):
         result = []
@@ -18,23 +19,54 @@ class Plotter():
         for i in value_list:
             temp = i / float(max(z))
             temp2 = temp * self.height - 1
-            result.append(int(temp2))
+            result.append((i, int(temp2)))
         return result
 
     def generate_matrix(self, values):
         matrix = [[' ' for x in range(self.width)] for y in range(self.height)]
 
         for index, value in enumerate(values):
-            # print('i: ', index, ' v: ', value)
             matrix[value][index] = '*'
         return matrix
 
-    def print_matrix(self, matrix):
-        #print('y_length: ', len(matrix))
-        #print('x_length: ', len(matrix[0]))
+    def print_matrix(self, y_scale, matrix):
 
+        self.print_line(self.width)
         for point_y in reversed(range(len(matrix))):
+            print(y_scale[point_y], end='')
             for point_x in range(len(matrix[0])):
                 #print(point_x, ',', point_y)
                 print(matrix[point_y][point_x], end='')
             print('')
+        self.print_line(self.width)
+
+    def get_y_scale(self, values):
+        original_values = [original for (original, scaled) in values]
+        is_originals_floats = isinstance(original_values[0], float)
+        scaled_values = [scaled for (original, scaled) in values]
+        original_max = max(original_values)
+        original_min = min(original_values)
+        original_diff = original_max - original_min
+        scaled_max = max(scaled_values)
+        scaled_min = min(scaled_values)
+        scaled_diff = scaled_max - scaled_min + 1
+
+        scale = original_diff / scaled_diff
+        y_scale = [0 for x in range(self.height)]
+        next_value = original_max
+        for index in reversed(range(self.height)):
+            if is_originals_floats:
+                value_string = '{0:.2f} '.format(next_value)
+                y_scale[index] = value_string
+            else:
+                value_string = str(next_value)
+                y_scale[index] = value_string.rjust(10)
+            next_value -= scale
+
+        return y_scale
+
+
+    def print_line(self, length):
+        for index in range(length):
+            print('-', end='')
+        print()
